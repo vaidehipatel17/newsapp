54769813e196f54cc80b2198d382cebea701fe67
vaidehipatel17 <vaidehipatel1710@gmail.com>
2022-03-13 18:25:25 -0400

    My first commit

---
+++ src/components/News.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+    constructor(){
+        super();
+        this.state={
+            articles: [],
+            loading: false,
+            page: 1
+
+        }
+    }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=b856ff188fc2490baab34d7bb36fd358";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles:parsedData.articles, totalArticles: parsedData.totalResults})
+    }
+
+    handlePrevClick = async ()=>{
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b856ff188fc2490baab34d7bb36fd358&page=${this.state.page - 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            
+        this.setState({
+            page: this.state.page -1,
+            articles: parsedData.articles
+        })
+    }
+   
+    handleNextClick = async ()=>{
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }
+        else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b856ff188fc2490baab34d7bb36fd358&page=${this.state.page + 1}&pageSize=20`;
+                let data = await fetch(url);
+                let parsedData = await data.json(); 
+            this.setState({
+                page: this.state.page +1,
+                articles: parsedData.articles
+            })
+                
+         }
+    }
+   
+  render() {
+    return (
+        <div className='container'>
+         <h2>News Top Headlins</h2>
+            <div className="row">
+                {this.state.articles.map((element)=>{
+                    return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element?element.title:""} description={element?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                        </div>
+                })}
+                
+                
+            </div>
+            <div className='container d-flex justify-content-between'>
+            <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+            <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+            </div>
+        </div>
+        
+    )
+  }
+}
+
+export default News
